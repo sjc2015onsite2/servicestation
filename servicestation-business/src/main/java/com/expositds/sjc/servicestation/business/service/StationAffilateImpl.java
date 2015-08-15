1d7c48619f9a5c5d8358ba1c8ff13726ddf0425f
Алексей Суслов <alexei.suslov@gmail.com>
2015-08-16 00:02:11 +0300

    StationAffilateImpl complete

--- servicestation-business/src/main/java/com/expositds/sjc/servicestation/business/service/StationAffilateImpl.java
+++ servicestation-business/src/main/java/com/expositds/sjc/servicestation/business/service/StationAffilateImpl.java
@@ -1,9 +1,17 @@
 package com.expositds.sjc.servicestation.business.service;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.expositds.sjc.servicestation.business.repository.dao.AffilateDao;
+import com.expositds.sjc.servicestation.business.repository.dao.OrderDao;
+import com.expositds.sjc.servicestation.business.repository.entity.AffilateEntity;
+import com.expositds.sjc.servicestation.business.repository.entity.OrderEntity;
+import com.expositds.sjc.servicestation.business.repository.entity.PersonEntity;
+import com.expositds.sjc.servicestation.business.repository.tools.BasicEntityModelObjectConverter;
 import com.expositds.sjc.servicestation.domain.model.Affilate;
 import com.expositds.sjc.servicestation.domain.model.Order;
 import com.expositds.sjc.servicestation.domain.model.Person;
@@ -15,17 +23,38 @@ import com.expositds.sjc.servicestation.domain.service.StationAffilate;
  */
 @Service
 public class StationAffilateImpl implements StationAffilate {
+	
+	@Autowired
+	private AffilateDao affilateDao;
+	
+	@Autowired
+	private OrderDao orderDao;
+	
+	@Autowired
+	private BasicEntityModelObjectConverter entityModelObjectConverter;
 
 	@Override
 	public void deleteOrder(Affilate affilate, Order order) {
-		// TODO Auto-generated method stub
+		AffilateEntity affilateEntity = affilateDao.findById(affilate.getAffilateId());
+		OrderEntity orderEntity = orderDao.findById(order.getOrderId());
+		
+		affilateEntity.getOrders().remove(orderEntity);
+		
+		affilateDao.update(affilateEntity);
 
 	}
 
 	@Override
 	public Set<Person> getAffiliateMechanics(Affilate affiliate) {
-		// TODO Auto-generated method stub
-		return null;
+		AffilateEntity affilateEntity = affilateDao.findById(affiliate.getAffilateId());
+		Set<PersonEntity> mechanicsEntity = affilateEntity.getPersons();
+		
+		Set<Person> mechanics = new HashSet<>();
+		for (PersonEntity currentMechanicEntity : mechanicsEntity) {
+			mechanics.add((Person) entityModelObjectConverter.convert(currentMechanicEntity, Person.class));
+		}
+		
+		return mechanics;
 	}
 
 }
