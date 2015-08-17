04fa6149eac30d8e9afbf8a8bb8cb0fede4de95c
Сергей <@Serg-pc>
2015-08-17 20:55:10 +0300

    Logout

--- servicestation-app/src/main/java/com/expositds/sjc/servicestation/app/CustomerOrdersController.java
+++ servicestation-app/src/main/java/com/expositds/sjc/servicestation/app/CustomerOrdersController.java
@@ -1,52 +1,50 @@
 package com.expositds.sjc.servicestation.app;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.expositds.sjc.servicestation.domain.model.Logginer;
+import com.expositds.sjc.servicestation.domain.model.Order;
 import com.expositds.sjc.servicestation.domain.model.SiteUser;
+import com.expositds.sjc.servicestation.domain.model.Station;
 import com.expositds.sjc.servicestation.domain.service.AuthorizedUserSite;
 import com.expositds.sjc.servicestation.domain.service.Identification;
 
 /**
  * <b>CustomerOrdersController</b>
- * Отвечает за отображение страницы заявок клиента (www.site-agregator.com/myorders)
  * 
- * @see <a href="https://bitbucket.org/sjc2015onsite2/service-station-domain/wiki/RU/Пользовательский%20интерфейс/Пользователь%20сайта/Мои%20заявки">Пользователь сайта / Мои заявки</a>
- * 
- * @author Sergey Rybakov, Oleg Ryzhko
+ * @author Sergey Rybakov
  * */
 
 
 @Controller
 public class CustomerOrdersController {
 
-	//@Autowired
-	//private AuthorizedUserSite authorizedUserSiteService;
+	@Autowired
+	private AuthorizedUserSite authorizedUserSiteService;
 	
-	/*@RequestMapping(value = "/myorders", method = { RequestMethod.GET })
-	public ModelAndView myorders(@PathVariable("userId") SiteUser user, HttpSession session) {
-		
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("orders", authorizedUserSiteService.getOrders(user));
-		mav.setViewName("myOrders");
-		return mav;
-	}*/
+	@Autowired
+	private Identification identificationService;
 	
-	@RequestMapping(value = "/myorders", method = { RequestMethod.GET })
-	public String myorders() {
-		
-		ModelAndView mav = new ModelAndView();	
-		return "myOrders";
-	}
+		@RequestMapping(value = "/myorders", method = { RequestMethod.GET })
+		public ModelAndView myorders(Authentication auth) {
+			
+			Logginer logginer = identificationService.getLogginerByName(auth.getName());
+			SiteUser user = identificationService.getSiteUserById(logginer.getId().toString());
+			
+			ModelAndView mav = new ModelAndView();
+			mav.addObject("orders", authorizedUserSiteService.getOrders(user));
+			mav.setViewName("myOrders");
+			return mav;
+		}
 	
 	
-	@RequestMapping(value = "/myorders/orderId", method = { RequestMethod.GET })
-	public String showCustomerOrderData() {
-		
-	return "customer.order.data";
-	}
+	
 }
