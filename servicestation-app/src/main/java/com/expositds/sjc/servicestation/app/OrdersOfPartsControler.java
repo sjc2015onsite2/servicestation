814db2e53ab64ea2792770638141c5745be0fe10
Сергей <@Serg-pc.exposit-ds.com>
2015-08-26 12:07:07 +0300

    Смена статуса у заявок на детали и сортировка заявок

--- servicestation-app/src/main/java/com/expositds/sjc/servicestation/app/OrdersOfPartsControler.java
+++ servicestation-app/src/main/java/com/expositds/sjc/servicestation/app/OrdersOfPartsControler.java
@@ -3,18 +3,22 @@ package com.expositds.sjc.servicestation.app;
 import java.text.SimpleDateFormat;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.expositds.sjc.servicestation.app.dto.PartOrderDto;
 import com.expositds.sjc.servicestation.domain.model.Logginer;
+import com.expositds.sjc.servicestation.domain.model.OrderStatus;
 import com.expositds.sjc.servicestation.domain.model.Part;
 import com.expositds.sjc.servicestation.domain.model.PartOrder;
+import com.expositds.sjc.servicestation.domain.model.PartOrderStatus;
 import com.expositds.sjc.servicestation.domain.model.Person;
 import com.expositds.sjc.servicestation.domain.model.Affilate; 
 import com.expositds.sjc.servicestation.domain.service.Identification;
@@ -37,7 +41,7 @@ public class OrdersOfPartsControler {
 	private Mechanic mechanicService;
 	
 	@RequestMapping(value = "/mechanic/partsorders", method = RequestMethod.GET)
-	public ModelAndView showListOfSpareParts(Authentication auth) {
+	public ModelAndView showListOfPartsOrders(Authentication auth) {
 		
 		Logginer logginer = identificationService.getLogginerByName(auth.getName());
 		Person mechanic = identificationService.getPersonById(logginer.getId().toString());
@@ -46,16 +50,28 @@ public class OrdersOfPartsControler {
 		Set<PartOrder> partsorders = new HashSet<>();
 		partsorders = mechanicService.getPartOrders(affiliate);
 		
-		Set<PartOrderDto> partOrdersDto = new HashSet<>();
+		Set<PartOrderDto> partOrdersDto = new TreeSet<>();
 		
 		for (PartOrder currentorder : partsorders){
 			partOrdersDto.add(new PartOrderDto(currentorder));
 		}
-		
 	
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("partOrdersDto", partOrdersDto);
+		mav.addObject("partOrderStatuses", PartOrderStatus.values());
 		mav.setViewName("parts.orders.list");
+	return mav;
+	}
+	
+	@RequestMapping(value = "/mechanic/partsorders", method = RequestMethod.POST)
+	public ModelAndView changeStatusOfPartsOrder(
+			@RequestParam(value = "orderId") PartOrder partOrder) {
+		PartOrderStatus partOrderStatus = PartOrderStatus.READY;
+		mechanicService.setPartOrderStatus(partOrder, partOrderStatus);
+		
+	
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:/mechanic/partsorders");
 	return mav;
 	}
 }
