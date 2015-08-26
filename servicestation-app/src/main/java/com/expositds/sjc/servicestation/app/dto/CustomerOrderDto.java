96658045b3bffeb107d77da1fb87802df6aae141
Алексей Суслов <alexei.suslov@gmail.com>
2015-08-26 11:43:37 +0300

    CustomersOrderDto ready

--- servicestation-app/src/main/java/com/expositds/sjc/servicestation/app/dto/CustomerOrderDto.java
+++ servicestation-app/src/main/java/com/expositds/sjc/servicestation/app/dto/CustomerOrderDto.java
@@ -2,13 +2,9 @@ package com.expositds.sjc.servicestation.app.dto;
 
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.expositds.sjc.servicestation.business.service.IdentificationImpl;
 import com.expositds.sjc.servicestation.domain.model.Order;
 import com.expositds.sjc.servicestation.domain.model.Part;
 import com.expositds.sjc.servicestation.domain.model.Service;
@@ -16,6 +12,11 @@ import com.expositds.sjc.servicestation.domain.model.Station;
 import com.expositds.sjc.servicestation.domain.service.AuthorizedUserSite;
 import com.expositds.sjc.servicestation.domain.service.Identification;
 
+/**
+ * @author Alexey Suslov
+ * @author Sergey Rybakov
+ *
+ */
 public class CustomerOrderDto {
 	
 	private String orderId;
@@ -42,60 +43,19 @@ public class CustomerOrderDto {
 	
 	private Map<String, String> stationsIdAndNames;
 	
-	@Autowired
-	Identification identification;
-	
-	@Autowired
-	AuthorizedUserSite authorizedUserSite;
-	
-
-	public CustomerOrderDto(Order order) {
-		
-		this.orderId = order.getOrderId().toString();
-		
-		this.stationName = identification.getStationByOrder(order).getName().toString();
-		
-		this.mechanicName = identification.getMechanicByOrder(order).getName().toString();
-		
-		this.problemDescription = order.getProblemDescription().toString();
-		
-		this.orderStatus = order.getStatus().toString();
-		
-		this.orderCost = new String();
-		
-		this.serviceRows = new String[order.getServices().size()][4];
-		int i = 0;
-		for(Service currentService : order.getServices().keySet()){
-			serviceRows[i][0] = currentService.getName();
-			serviceRows[i][1] = order.getServices().get(currentService).toString();
-			serviceRows[i][2] = order.getOrderServicesPriceList().get(currentService).toString();
-			serviceRows[i][3] = Integer.toString((Integer.parseInt(serviceRows[i][1]) * Integer.parseInt(serviceRows[i][2])));
-			orderCost += Integer.parseInt(serviceRows[i][3]);
-			i++;
-		}
-		
-		this.partsNamesAndQuantity = new HashMap<>();
-		for(Part currentPart : order.getParts().keySet()){
-			partsNamesAndQuantity.put(currentPart.getName().toString(), order.getParts().get(currentPart).toString());
-		}
-		
-		this.completedDate = new String();
-		this.createdDate = new String();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-		if(order.getCompleteDate() != null)
-			completedDate = dateFormat.format(order.getCompleteDate().getTime());
-		createdDate = dateFormat.format(order.getCreateDate().getTime());
-		
-		
-		this.notificationMessage = new String();
-		if(order.getNotification() != null){
-			notificationMessage = order.getNotification().getMessage().toString();
-		}
-		
-		this.stationsIdAndNames = new HashMap<>();
-		for(Station currentStation : authorizedUserSite.getServiceStations()){
-			stationsIdAndNames.put(currentStation.getStationId().toString(),currentStation.getName().toString());
-		}
+	public CustomerOrderDto(Builder builder) {
+		orderId = builder.getOrderId();
+		stationName = builder.getStationName();
+		mechanicName = builder.getMechanicName();
+		problemDescription = builder.getProblemDescription();
+		orderStatus = builder.getOrderStatus();	
+		serviceRows = builder.getServiceRows();
+		orderCost = builder.getOrderCost();
+		partsNamesAndQuantity = builder.getPartsNamesAndQuantity();
+		completedDate = builder.getCompletedDate();
+		createdDate = builder.getCreatedDate();
+		notificationMessage = builder.getNotificationMessage();
+		stationsIdAndNames = builder.getStationsIdAndNames();
 	}
 
 	public String getOrderId() {
@@ -142,5 +102,106 @@ public class CustomerOrderDto {
 		return stationsIdAndNames;
 	}
 	
-	
+	public String[][] getServiceRows() {
+		return serviceRows;
+	}
+
+
+	public static class Builder {
+		
+		private Order order;
+		
+		private Integer orderCost;
+		
+		private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+		
+		@Autowired
+		private Identification identification;
+		
+		@Autowired
+		AuthorizedUserSite authorizedUserSite;
+		
+		public Builder(Order order) {
+			this.order = order;
+		}
+		
+		public String getOrderId() {
+			return order.getOrderId().toString();
+		}
+		
+		public String getStationName() {
+			return identification.getStationByOrder(order).getName().toString();
+		}
+		
+		public String getMechanicName() {
+			return identification.getMechanicByOrder(order).getName().toString();
+		}
+		
+		public String getProblemDescription() {
+			return order.getProblemDescription().toString();
+		}
+		
+		public String getOrderStatus() {
+			return order.getStatus().toString();
+		}
+		
+		public String[][] getServiceRows() {
+			
+			String [][] serviceRows = new String[order.getServices().size()][4];
+			int i = 0;
+			for(Service currentService : order.getServices().keySet()){
+				serviceRows[i][0] = currentService.getName();
+				serviceRows[i][1] = order.getServices().get(currentService).toString();
+				serviceRows[i][2] = order.getOrderServicesPriceList().get(currentService).toString();
+				serviceRows[i][3] = Integer.toString((Integer.parseInt(serviceRows[i][1]) * Integer.parseInt(serviceRows[i][2])));
+				orderCost += Integer.parseInt(serviceRows[i][3]);
+				i++;
+			}
+			
+			return serviceRows;
+		}
+		
+		public Map<String, String> getPartsNamesAndQuantity() {
+			Map<String, String> result = new HashMap<>();
+			for(Part currentPart : order.getParts().keySet()){
+				result.put(currentPart.getName().toString(), order.getParts().get(currentPart).toString());
+			}
+			return result;
+		}
+		
+		public String getOrderCost() {
+			return orderCost.toString();
+		}
+		
+		public String getCompletedDate() {
+			if(order.getCompleteDate() != null)
+				return dateFormat.format(order.getCompleteDate().getTime());
+			return new String();
+		}
+		
+		public String getCreatedDate() {
+			if(order.getCreateDate() != null)
+				return dateFormat.format(order.getCreateDate().getTime());
+			return new String();
+		}
+		
+		public String getNotificationMessage() {
+			if(order.getNotification() != null)
+				return order.getNotification().getMessage().toString();
+			return new String();
+		}
+		
+		public Map<String, String> getStationsIdAndNames() {
+			Map<String, String> result = new HashMap<>();
+			for(Station currentStation : authorizedUserSite.getServiceStations()){
+				result.put(currentStation.getStationId().toString(), currentStation.getName());
+			}
+			
+			return result;
+		}
+		
+		public CustomerOrderDto build() {
+			return new CustomerOrderDto(this);
+		}
+	}
 }
