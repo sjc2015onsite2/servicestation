69878908f55183dbae22c9a43053af11c6ac7265
Сергей <@Serg-pc.exposit-ds.com>
2015-08-26 13:47:37 +0300

    StationData

--- servicestation-app/src/main/java/com/expositds/sjc/servicestation/app/StationDataController.java
+++ servicestation-app/src/main/java/com/expositds/sjc/servicestation/app/StationDataController.java
@@ -1,7 +1,10 @@
 package com.expositds.sjc.servicestation.app;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.expositds.sjc.servicestation.domain.model.Logginer;
+import com.expositds.sjc.servicestation.domain.model.Person;
+import com.expositds.sjc.servicestation.domain.model.Station;
+import com.expositds.sjc.servicestation.domain.service.Accountant;
+import com.expositds.sjc.servicestation.domain.service.Identification;
 
 /**
  * <b>CustomerOrdersController</b>
@@ -24,10 +33,40 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/accountant")
 public class StationDataController {
 	
+	@Autowired 
+	private Accountant accountantService;
+	
+	@Autowired
+	private Identification identificationService;
+	
 		@RequestMapping(value = "/station", method = RequestMethod.GET)
-		public ModelAndView myorders() {
+		public ModelAndView showStationData() {
 			
 			ModelAndView mav = new ModelAndView();
+			mav.setViewName("station");
+			return mav;
+		}
+		
+		@RequestMapping(value = "/station", method = RequestMethod.POST)
+		public ModelAndView selectDate(
+				Authentication auth,
+				@RequestParam String startdate,
+				@RequestParam String finishdate) throws ParseException {
+			
+			Logginer logginer = identificationService.getLogginerByName(auth.getName());
+			Person accountant = identificationService.getPersonById(logginer.getId().toString());
+			
+			Station station = identificationService.getStationByPerson(accountant);
+			
+			Calendar sDate = Calendar.getInstance();
+			Calendar fDate = Calendar.getInstance();
+			SimpleDateFormat sdf = (SimpleDateFormat) new SimpleDateFormat("dd.MM.yyyy", Locale.US);
+			sDate.setTime(sdf.parse(startdate));
+			fDate.setTime(sdf.parse(finishdate));
+			
+			ModelAndView mav = new ModelAndView();
+			mav.addObject("profit", accountantService.getServiceStationProfit(station, sDate, fDate));
+			mav.addObject("expenses", accountantService.getServiceStationCharges(station, sDate, fDate));
 			mav.setViewName("station");
 			return mav;
 		}
--- servicestation-app/src/main/webapp/WEB-INF/views/accountant/station.jsp
+++ servicestation-app/src/main/webapp/WEB-INF/views/accountant/station.jsp
@@ -1,3 +1,45 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<form action="../accountant/station" method='POST'>
+	<div class="col-sm-offset-2 col-sm-8">
+		<div class="col-sm-2">
+			<strong class="text-right">Enter startDate: </strong>
+		</div>
+		<div class="col-sm-3">
+			<input name="startdate" type="text" class="form-control input-sm" placeholder="dd.MM.yyyy">
+		</div>
+		<div class="col-sm-2">
+		</div>
+		<div class="col-sm-2">
+			<strong class="text-right">Enter finishDate: </strong>
+		</div>
+		<div class="col-sm-3">
+			<input name="finishdate" type="text" class="form-control input-sm" placeholder="dd.MM.yyyy">
+		</div>
+	</div>
+	<div>
+		<div class="col-sm-offset-5 col-sm-3">
+			<br />
+			<br />
+			<button type="submit" class="btn btn-warning btn-sm">Show profit and expenses</button>
+		</div>
+		<div class="col-sm-4"></div>
+	</div>
+</form>
+<div>
+	<div class="col-sm-offset-2 col-sm-4">
+		<br />
+		<br />
+		
+		<strong>Profit: ${profit}</strong>
+	</div>
+	<div class="col-sm-offset-6 col-sm-6"></div>
+</div>
+<div>
+	<div class="col-sm-offset-2 col-sm-4">
+		<br />
+		<strong>Expenses: ${expenses}</strong>
+	</div>
+</div>
